74167390fb92aaeb341343b23b880f3effa3de3d
Edgar Guamo M <edgarguamo@gmail.com>
2021-06-16 11:23:23 -0500

    update nvim config and create fish config

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+set -gx TERM xterm-256color
+set -gx EDITOR nvim
+set -gx EDITOR nvim
+
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+
+# NodeJS
+set -gx PATH node_modules/.bin $PATH
+
+#Alias
+alias v "nvim"
